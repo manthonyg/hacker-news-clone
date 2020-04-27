463bd76c21c9768d9c72c51ed51afca4e60e34da
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-04-27 18:18:15 +1000

    project setup w/ eslint, pret, webpack, deps

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html'
+    })
+  ]
+};
