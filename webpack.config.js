c1dc52a6ebf9c5fde5cb2532ae0b07f1efa7f276
Michael Grandori <41161951+manthonyg@users.noreply.github.com>
2020-06-11 21:21:26 +1000

    added redirects for prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './client/index.js',
@@ -16,15 +17,21 @@ module.exports = {
       { test: /\.(css)$/, use: ['style-loader', 'css-loader'] }
     ]
   },
-  mode: 'development',
   devServer: {
     historyApiFallback: true
   },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [
-    // strips all locales except en
+    /**
+     * @description MomentLocalesPlugin strips all locales except en
+     * */
+
     new MomentLocalesPlugin(),
     new HTMLWebpackPlugin({
       template: './client/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [{ from: '_redirects', to: path.resolve(__dirname, 'dist') }]
     })
   ]
 };
